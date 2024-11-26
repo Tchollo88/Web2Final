57bcd6d611e20f894df5b1a33a64daf67648f3dc
Tchollo88 <Tcholloman@gmail.com>
2024-11-26 09:49:24 -0800

    Add project files.

---
+++ Holloman_Web2Final/Data/Holloman_Web2FinalDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Holloman_Web2Final.Models;
+
+namespace Holloman_Web2Final.Data
+{
+    public class Holloman_Web2FinalDb : DbContext
+    {
+        public Holloman_Web2FinalDb (DbContextOptions<Holloman_Web2FinalDb> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Holloman_Web2Final.Models.Customer> Customer { get; set; } = default!;
+    }
+}
